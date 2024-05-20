753c9909a2a207ef3f8283e2d03c7e7fd8d50de9
TY <YAKUBTALIB70@GMAIL.COM>
2024-05-20 13:56:47 +0300

    added auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                
+                <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+  <div class="card-header">Total students </div>
+  <div class="card-body">
+    
+    <p class="card-text">{{students}}</p>
+  </div>     
+           
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\StudentController;
 
-Route::get('', [StudentController::class, 'index'])->name('index');
-Route::get('/create',[StudentController::class,'create']);
-Route::post('/create',[StudentController::class,'store'])->name('add');
+Route::get('/view', [StudentController::class, 'index'])->name('view') ->middleware('auth');
+Route::get('/create',[StudentController::class,'create']) -> middleware('auth');
+Route::post('/create',[StudentController::class,'store'])->name('add') -> middleware('auth');
 Route::get('/delete/{id}',[StudentController::class,'delete'])->name('delete');
 Route::get('/edit/{id}',[StudentController::class,'edit'])->name('edit');
 Route::post('/update/{id}',[StudentController::class,'update'])->name('update');
 
     
     
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
